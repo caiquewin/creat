d6d352ce1bfe616ccbed80efeb1fe5aae7b664d2
caiquewin <49238669+caiquewin@users.noreply.github.com>
2020-01-15 18:54:46 -0300

    Add files via upload

---
+++ HelloWord/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace HelloWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.primeiro_botao = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // primeiro_botao
+            // 
+            this.primeiro_botao.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.primeiro_botao.ForeColor = System.Drawing.Color.Red;
+            this.primeiro_botao.Location = new System.Drawing.Point(270, 78);
+            this.primeiro_botao.Name = "primeiro_botao";
+            this.primeiro_botao.Size = new System.Drawing.Size(96, 37);
+            this.primeiro_botao.TabIndex = 0;
+            this.primeiro_botao.Text = "Desligar";
+            this.primeiro_botao.UseVisualStyleBackColor = false;
+            this.primeiro_botao.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(28, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cancelar o Desligamento";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.primeiro_botao);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 300);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button primeiro_botao;
+        private readonly EventHandler Form1_Load;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HelloWord/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWord
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("Seu computador vai desliguar");
+            Process.Start("Shutdown", "-s -f -t 3600");
+            
+
+
+        }
+       
+        private void button1_Click_2(object sender, EventArgs e) // botão de cancelar o desligamento
+        {
+
+            DialogResult result2 = MessageBox.Show("Deseja cancelar a programação ",// aqui vai aparecer uma mensagem se desejar cancelar realmente
+            "The Question",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question);
+            if (result2 == DialogResult.Yes)
+            {
+                Process.Start("Shutdown", "-a");
+                MessageBox.Show("Seu Desligamento Foi Cancelado");
+                
+            }
+            if (result2 == DialogResult.No)
+            {
+                MessageBox.Show("Cancelado");
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+    }
+}
